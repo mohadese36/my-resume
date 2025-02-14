311098b591e4d722487b29c5aa13c7400d2657c5
mohadese36 <ariagh3936@gmail.com>
2025-02-14 15:34:24 +0330

    Add link to v2-info.html

---
+++ app.js/info-2.js
@@ -0,0 +1,89 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+      const sections = document.querySelectorAll("section"); // گرفتن تمام سکشن‌ها
+      const navLinks = document.querySelectorAll(".nav-link"); // لینک‌های منو
+    
+      function updateActiveLink() {
+        let index = sections.length;
+    
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+    
+        navLinks.forEach((link) => link.classList.remove("active")); // حذف کلاس از همه لینک‌ها
+        navLinks[index]?.classList.add("active"); // افزودن کلاس به لینک مرتبط
+      }
+    
+      updateActiveLink();
+      window.addEventListener("scroll", updateActiveLink);
+    });
+    
+    
+
+
+
+    // رویداد هاور برای تاچ ها
+
+    const experienceCard = document.querySelectorAll('.experience-card');
+    const projectBox = document.querySelectorAll('.project-box');
+    
+    // اضافه کردن کلاس touch-hover هنگام لمس
+    const addTouchHover = (element) => {
+      element.classList.add('touch-hover');
+    };
+    
+    // حذف کلاس touch-hover هنگام برداشتن انگشت
+    const removeTouchHover = (element) => {
+      element.classList.remove('touch-hover');
+    };
+    
+    // برای شبیه‌سازی حالت hover در دستگاه‌های لمسی
+    experienceCard.forEach(card => {
+      card.addEventListener('touchstart', () => addTouchHover(card)); // لمس برای شروع
+      card.addEventListener('touchend', () => removeTouchHover(card)); // لمس برای پایان
+    });
+    
+    projectBox.forEach(box => {
+      box.addEventListener('touchstart', () => addTouchHover(box)); // لمس برای شروع
+      box.addEventListener('touchend', () => removeTouchHover(box)); // لمس برای پایان
+    });
+    
+
+    
+
+
+    //افکت موس
+
+    const colors = [
+      "rgba(0, 255, 255, 0.3)",   // آبی نئونی
+      // "rgba(180, 0, 255, 0.7)",   // بنفش نئونی
+      // "rgba(255, 0, 150, 0.7)",   // صورتی نئونی
+      // "rgba(255, 215, 0, 0.1)",   // طلایی نئونی
+      // "rgba(0, 255, 100, 0.7)",
+      // "rgba(128, 0, 128, 0.8)" ,    //بنفش سلطنتی 👑
+      //  "rgba(70, 130, 180, 0.3)" ,       // آبی یاقوتی 🔷
+      // "rgba(80, 200, 120, 0.8)",  //سبز زمردی 💎
+      // "rgba(192, 192, 192, 0.8)",     //نقره‌ای متالیک 
+      // "rgba(255, 185, 0, 0.8)",       //طلایی متالیک 💛	
+  
+  ];
+  
+  document.addEventListener("mousemove", (e) => {
+      let trail = document.createElement("div");
+      trail.classList.add("trail");
+      document.body.appendChild(trail);
+  
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      trail.style.background = randomColor;
+      trail.style.filter = `drop-shadow(0 0 10px ${randomColor})`;
+  
+      trail.style.left = `${e.pageX}px`;
+      trail.style.top = `${e.pageY}px`;
+  
+      setTimeout(() => {
+          trail.style.transform = "scale(2)";
+          trail.style.opacity = "0";
+      }, 10);
+  
+      setTimeout(() => {
+          trail.remove();
+      }, 500);
+  });
---
+++ app.js/lang-2.js
@@ -0,0 +1,114 @@
+
+
+let currentLanguage = localStorage.getItem('lang') || 'en'; 
+
+
+const changeLanguageButton = document.getElementById('change-language');
+
+function defineLanguageAttributes(lang) {
+
+  document.documentElement.setAttribute('lang', lang);
+  document.body.classList.toggle('rtl', lang === 'fa');
+  document.body.classList.toggle('ltr', lang === 'en');
+  
+
+  if (changeLanguageButton) {
+    changeLanguageButton.innerHTML = `<img src="images/lang-btn.png" alt="lang-btn"><span>${lang === 'fa' ? 'English' : 'Persian'}</span>`;
+  } else {
+    console.warn("دکمه تغییر زبان پیدا نشد!");
+  }
+}
+
+const loadLanguage = (lang) => {
+  fetch(`lang/${lang}.json`)
+    .then(response => response.json())
+    .then(data => {
+
+      document.getElementById('name').textContent = data.sidebar.name;
+      document.getElementById('position').textContent = data.sidebar.position;
+      document.getElementById('description').innerHTML = data.sidebar.description;
+
+
+      ['about', 'experience', 'projects'].forEach(linkID => {
+        const navLink = document.getElementById(`navbar-${linkID}`);
+        if (navLink) navLink.innerHTML = `${data.headerNav[linkID]}`;
+      });
+  
+            const navLinks = ['about', 'experience', 'projects'];
+            navLinks.forEach(linkId => {
+              const navLink = document.getElementById(`nav-${linkId}`);
+              navLink.innerHTML = `<span></span>${data.nav[linkId]}`; 
+            });
+
+      document.getElementById('social-github').href = data.social.github;
+      document.getElementById('social-linkedin').href = data.social.linkedin;
+      document.getElementById('social-instagram').href = data.social.instagram;
+
+
+      document.getElementById('about-heading').textContent = data.about.title;
+      document.getElementById('about-first-paragraph').textContent = data.about.firstParagraph;
+      document.getElementById('about-second-paragraph').textContent = data.about.secondParagraph;
+      document.getElementById('about-third-paragraph').textContent = data.about.thirdParagraph;
+      document.getElementById('about-fourth-paragraph').textContent = data.about.fourthParagraph;
+
+
+      document.getElementById('experience-title').textContent = data.experience.title;
+
+
+      const experienceCards = document.querySelectorAll('.experience-card');
+      experienceCards.forEach((card, index) => {
+        if (index < data.experience.cards.length) {
+          card.querySelector('h3').textContent = data.experience.cards[index].title;
+
+          const iconSpan = document.createElement('span');
+          iconSpan.classList.add('material-symbols-outlined', 'arrow');
+          iconSpan.textContent = data.experience.cards[index].icon;
+          card.querySelector('h3').appendChild(iconSpan);
+
+          const subtitles = card.querySelectorAll('.experience-card-title p');
+          if (subtitles.length > 0) subtitles[0].textContent = data.experience.cards[index].subtitle1;
+          if (subtitles.length > 1) subtitles[1].textContent = data.experience.cards[index].subtitle2;
+          card.querySelector('p[id="card-description"]').textContent = data.experience.cards[index].description;
+        }
+      });
+
+      document.getElementById('resume-link').innerHTML = data.experience.viewResume;
+
+
+      document.getElementById('projects-title').textContent = data.projects.title;
+
+
+      const projectCards = document.querySelectorAll('.project-box');
+      projectCards.forEach((card, index) => {
+        if (index < data.projects.cards.length) {
+          card.querySelector('h3').innerHTML = data.projects.cards[index].title;
+          card.querySelector('p').innerHTML = data.projects.cards[index].description;
+          card.querySelector('.project-stats span').innerHTML = data.projects.cards[index].stats;
+        }
+      });
+
+      document.querySelector('.archive-project-container a').innerHTML = data.projects.moreInfo;
+
+ 
+      document.getElementById('footer-text').innerHTML = data.footer.text;
+
+      defineLanguageAttributes(lang);
+    })
+    .catch(error => console.error('Error loading language:', error));
+};
+
+
+document.getElementById('change-language').addEventListener('click', () => {
+  currentLanguage = (currentLanguage === 'en') ? 'fa' : 'en';
+  localStorage.setItem('lang', currentLanguage); 
+  loadLanguage(currentLanguage);
+});
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   loadLanguage(currentLanguage);
+// });
+
+window.onload = function() {
+  loadLanguage(currentLanguage);
+};
